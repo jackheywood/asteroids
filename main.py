09a76aaa36268885b9f412d74438650f6a0b7a78
jackheywood <jackaheywood@gmail.com>
2025-02-05 21:28:15 +0000

    Set framerate to 60FPS

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main():
     print_startup()
 
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +18,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 def print_startup():
