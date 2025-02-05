baa6ed36dbf2b98d5ee51723d1cfa7535d33ecc7
jackheywood <jackaheywood@gmail.com>
2025-02-05 22:06:47 +0000

    Use pygame groups

--- player.py
+++ player.py
@@ -6,7 +6,7 @@ from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0
+        self.rotation = 180
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
