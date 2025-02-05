71c6fd9b33cacd1efaf20d160dcde0edff099ca1
jackheywood <jackaheywood@gmail.com>
2025-02-05 21:22:52 +0000

    Paint black screen

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import pygame
-
-import constants
 from constants import *
 
+
 def main():
+    print_startup()
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+def print_startup():
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
