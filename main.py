cbf33e87a54c712c418d8e49a77339ed55710976
jackheywood <jackaheywood@gmail.com>
2025-02-06 16:23:16 +0000

    Add asteroid destruction

--- main.py
+++ main.py
@@ -41,6 +41,11 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    asteroid.kill()
+                    shot.kill()
+
         screen.fill("black")
 
         for obj in drawable:
